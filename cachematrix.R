b8ce5f130c3578b33b8078392adb0d25fecb2501
JShea83 <jillianrayburn@yahoo.com>
2014-10-24 15:49:22 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This combination of functions will allow the inverse of a matrix to be
+## cached so the "expensive" calculation of a matrix inverse will only have
+## to be calculated one time. 
 
-## Write a short comment describing this function
+## This function will create a matrix object that can cache
+## the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ##Initialize the variable for the inverse.
+  mInv <- NULL
+  
+  ##Assign values for the matrix and its inverse.
+  set <- function(y) {
+        x<<-y
+        mInv<<-NULL
+  }
+  
+  ##Get the value of the matrix.
+  get<-function() x
+  
+  ##Assign the value of the matrix inverse.
+  setInverse <- function(inverse) mInv<<-inverse
+  
+  ##Get the value of the matrix inverse.
+  getInverse <- function() mInv
+  
+  ##Format functions in list.
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+  
 }
 
-
-## Write a short comment describing this function
+## This function will compute the inverse of the matrix object created by
+## the makeCacheMatrix function. If the inverse has already been computed
+## and stored in makeCacheMatrix, the cached value will be retrieved.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ##Get the value of the matrix inverse if it has already been calculated.
+  mInv<-x$getInverse()
+  
+  ##Check to see if inverse has been calculated and return cached result.
+  if(!is.null(mInv)) {
+    message("Getting cached data")
+    return(mInv)
+  }
+  
+  ##Get the values of the matrix.
+  data <- x$get()
+  
+  ##Calculate the inverse of the matrix.
+  mInv <- solve(data,...)
+  
+  ##Cache the value of the matrix inverse.
+  x$setInverse(mInv)
+ 
+  ## Return a matrix that is the inverse of 'x'
+  mInv
 }
